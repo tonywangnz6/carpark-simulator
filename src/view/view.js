17c961f3620abdc16d1ff09e44a47c87b517e876
tonywang <tonywangnz6@gmail.com>
2018-11-11 10:21:03 +1300

    Hot fix: add enter key listener for input box

--- src/view/view.js
+++ src/view/view.js
@@ -3,5 +3,12 @@ import { eventHandler } from './eventHandler.js';
 
 export let view = () => {
     const initial = init();
+
     document.getElementById("command-submit").addEventListener("click", () => eventHandler(initial));
+    document.getElementById("command-input").addEventListener("keyup", (event) => {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            eventHandler(initial);
+        }
+    });
 };
