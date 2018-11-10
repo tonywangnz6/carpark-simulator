5d4214fedf6c798b93335dff895dbb5b6f6ae211
tonywang <tonywangnz6@gmail.com>
2018-11-11 11:46:26 +1300

    Finished: add file read feature as input

--- src/view/eventHandler.js
+++ src/view/eventHandler.js
@@ -6,9 +6,9 @@ export let eventHandler = (initial) => {
       command = input.value.trim(),
       element = document.createElement('p');
     input.value = '';
-    while (output.hasChildNodes()) {
-        output.removeChild(output.firstChild);
-    }
+    // while (output.hasChildNodes()) {
+    //     output.removeChild(output.firstChild);
+    // }
     element.innerHTML = commandHandler(initial.dimensions, initial.bus, command);
     output.appendChild(element);
 };
--- src/view/view.js
+++ src/view/view.js
@@ -1,8 +1,10 @@
 import { init } from './init.js';
 import { eventHandler } from './eventHandler.js';
+import { file } from './fileReadHandler.js';
 
 export let view = () => {
     const initial = init();
+    document.getElementById('files').addEventListener('change', () => file(event, initial), false);
     document.getElementById("command-submit").addEventListener("click", () => eventHandler(initial));
     document.getElementById("command-input").addEventListener("keyup", (event) => {
         event.preventDefault();
