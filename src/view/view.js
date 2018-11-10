879daedba63ae7a4a9bdf319ec12ab567a14e4db
tonywang <tonywangnz6@gmail.com>
2018-11-11 10:16:37 +1300

    decouple view into different file

--- src/view/view.js
+++ src/view/view.js
@@ -1,20 +1,7 @@
-import { commandHandler } from './controller/commandHandler.js';
-import {Bus} from "./model/bus";
-import {Carpark} from "./model/carpark";
+import { init } from './init.js';
+import { eventHandler } from './eventHandler.js';
 
 export let view = () => {
-    let bus = new Bus(),
-        dimensions = new Carpark();
-    document.getElementById("command-submit").addEventListener("click", () => {
-        let input =  document.getElementById("command-input"),
-          output = document.getElementById("output"),
-          command = input.value.trim(),
-          element = document.createElement('p');
-        input.value = '';
-        while (output.hasChildNodes()) {
-            output.removeChild(output.firstChild);
-        }
-        element.innerHTML = commandHandler(dimensions, bus, command);
-        output.appendChild(element);
-    });
+    const initial = init();
+    document.getElementById("command-submit").addEventListener("click", () => eventHandler(initial));
 };
